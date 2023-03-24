94ebb07ed814d9d1acaa4914b28b7b817962a088
duckkle <pookachuthepook@gmail.com>
2023-03-24 15:32:43 +0000

    IT FINALLY WORKS, DON'T TOUCH IT K

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-import Vue from 'vue'
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
-import App from './App'
+import {createApp} from "vue"
+import App from "./app.vue";
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-Vue.use(BootstrapVue)
-Vue.use(BootstrapVueIcons)
-
-new Vue({ el: '#app', render: h => h(App) })
+const app = createApp(App);
+app.mount("#app");
